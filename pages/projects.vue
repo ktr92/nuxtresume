4c894317d3e97658c1183eb46ad5904b1a3123c8
ktr92 <kudashevtim@gmail.com>
2022-04-06 14:49:14 +0300

    master

--- pages/projects.vue
+++ pages/projects.vue
@@ -6,36 +6,9 @@
       </el-card>
       <br>
       <el-collapse v-model="activeNames" accordion v-for="(project, idx) in dataset" :key="idx" class="projects">
-        <el-collapse-item :name="idx">
-          <template slot="title" >
-              <div  class="projects__header clearfix">
-                <span class="projects__title">{{ project.prname }}</span>
-                <span class="projects__images">
-                  <span v-for="prtag in project.prtags" :key="prtag" class="tags-images">
-                    <span v-if="tags[prtag]">
-                      <img :src="require(`~/assets/img/${tags[prtag].icon}`)" />
-                      <span>{{ tags[prtag].name}} </span>
-                    </span>
-                  </span>
-                </span>
-              </div>
-               </template> 
-              <div class="text item projects__text">
-                 <p><vue-markdown>{{ project.prdescription }}</vue-markdown></p>
-              </div>
-              <el-button-group>
-                <el-button type="primary" v-if="project.prgit">
-                  <a :href="project.prgit" target="_blank">GitHub</a> 
-                </el-button>
-                <el-button type="primary" v-if="project.prlink">
-                  <a :href="project.prlink" target="_blank">Демо</a> 
-                </el-button>
-                <el-button type="primary" v-if="project.prwork">
-                  <a :href="project.prwork" target="_blank">Сайт</a> 
-                </el-button>
-              </el-button-group>
-              
-        </el-collapse-item>
+       
+          <AppProject :data="project" />
+         
       </el-collapse>  
   </div>
 </template>
@@ -49,22 +22,14 @@ export default {
   },
   data() {
     return {
-      tags: [],
-      activeNames: [false],
-      showthis: false,
-      projects: [],
+      activeNames: [false]
     }
-  },
-  mounted() {
-    this.tags = this.$store.getters['projects/tags']
   },
   computed: {
     ...mapState([
       'about'
     ])
   }
- 
- 
 }
 </script>
 
