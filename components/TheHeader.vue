967c596ca3ef84c2fcd4a21c0007298942d58c8c
Timur Kudashev <kudashev@ipotech.su>
2022-10-14 11:33:47 +0300

    make it better

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
    <el-menu class="el-menu-demo" mode="horizontal" :router="true" :default-active="activeIndex2">
-      <el-menu-item index="1" route="/">Резюме</el-menu-item>
-      <el-menu-item index="2" route="/projects">Портфолио</el-menu-item>
+      <router-link index="1" to="/"><div>Резюме</div></router-link>
+      <router-link index="2" to="/projects"><div>Портфолио</div></router-link>
     </el-menu>
   </div>
 </template>
@@ -34,10 +34,27 @@ export default {
     font-weight: 700;
     text-transform: uppercase;
 }
+  .el-menu-demo div {
+    float: left;
+    height: 60px;
+    line-height: 60px;
+    margin: 0;
+    border-bottom: 6px solid #fff;
+    color: #222;
+/*     background: $darkcolor;
+ */    width: 50%;
+    text-align: center;
+    font-size: 24px;
+    font-weight: 700;
+    text-transform: uppercase;
+}
 .el-menu--horizontal>.el-menu-item:hover, .el-menu--horizontal>.el-menu-item:focus, .el-menu--horizontal>.el-menu-item.is-active {
   color: #222;
-/*     background-color: $darkcolor;
- */    border-color: #409eff;
+   border-color: #409eff;
+}
+ .el-menu-demo .nuxt-link-exact-active div,  .el-menu-demo .nuxt-link-active div{
+  color: #222;
+   border-color: #409eff;
 }
 .el-header {
   padding: 0;
